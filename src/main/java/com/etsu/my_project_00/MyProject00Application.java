48a1d5af79721396c135695f8488d3fc26f900af
Etsu-source <jose.bonea@ug.edu.ec>
2024-11-18 23:43:58 -0500

    mi primer commit 

---
+++ src/main/java/com/etsu/my_project_00/MyProject00Application.java
@@ -0,0 +1,13 @@
+package com.etsu.my_project_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject00Application.class, args);
+	}
+
+}
